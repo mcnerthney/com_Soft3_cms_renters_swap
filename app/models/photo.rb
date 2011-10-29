6830c8ea993a298bd15670b93b3b1f561e2dd847
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-10-28 22:09:07 -0700

    start formatting screens

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,9 +4,6 @@ class Photo
   include Mongoid::Document
   include Mongoid::Timestamps
   field :title
-  field :width,  :type => Integer
-  field :height, :type => Integer
-  field :orientation
   field :position, :type => Integer, :default => 0
   embedded_in :item, :inverse_of => :photos
 
@@ -16,24 +13,11 @@ class Photo
   # Really no point if we don't have an image so we always require one
   validates_presence_of :image
 
-  before_validation :save_dimensions, :save_orientation, :save_position
+  before_validation :save_position
 
   scope :forward, order_by(:position.asc)
 
 private
-  def save_dimensions
-    if image.path
-      self.width  = Magick::Image::read(image.path).first.columns
-      self.height = Magick::Image::read(image.path).first.rows
-    end
-  end
-
-  def save_orientation
-    if image.path
-      self.orientation = (height.to_i > width.to_i) ? 'portrait' : 'landscape'
-    end
-  end
-
   def save_position
     self.position = (self._index + 1) if self.new_record?
   end
