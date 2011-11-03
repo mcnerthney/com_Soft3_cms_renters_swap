17b5d2a5ad4c57935779b6144c855a1d1d60d00a
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-11-02 22:12:13 -0700

    proeuction mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rent::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
     
+   config.action_mailer.default_url_options = { :host => 'www.rentersswap.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
         :address              => "smtpout.secureserver.net",
