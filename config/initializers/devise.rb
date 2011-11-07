833837206739655a2c853c7fdfb0d3e2243a2fa1
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-11-06 16:58:22 -0800

    fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,11 +198,11 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-    #if Rails.env.production?   
-    #config.omniauth :facebook, '248876391827927', '2389168ed3fbb8bed20f22a4a18ec625'
-    #else
+  if Rails.env.production?   
+    config.omniauth :facebook, '248876391827927', '2389168ed3fbb8bed20f22a4a18ec625'
+  else
     config.omniauth :facebook, '196690723742630', '6366295f10f5ff9d51a37f9f5567b283'
-    #end
+  end
     
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
